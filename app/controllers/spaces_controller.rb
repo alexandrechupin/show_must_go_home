62205b111b70e8a40427f24fd168a686ed11b24c
alexandrechupin <alexandrechupin@gmail.com>
2019-08-20 12:05:57 +0200

    bug fix

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,5 @@
 class SpacesController < ApplicationController
-  before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :set_space, only: [:show, :edit, :update, :destroy]
 
   def show
   end

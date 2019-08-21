86a39acafd3990a259138f6d0839bc7dfa999bd0
Ishan Bhojwani <ishanparis@gmail.com>
2019-08-21 12:06:48 +0200

    Convert user to host when user adds space

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -25,7 +25,8 @@ before_action :set_space, only: [:show, :edit, :update, :destroy]
     @space = Space.new(space_params)
     @space.user = current_user
     if @space.save
-      @space.user.host = true
+      current_user.host = true
+      current_user.save
       # appeler l'action update du controller users
       redirect_to space_path(@space)
     else
@@ -41,6 +42,6 @@ before_action :set_space, only: [:show, :edit, :update, :destroy]
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def space_params
-    params.require(:space).permit(:name, :street, :city, :zipcode, :event_type, :place_type, :capacity, :description, :photo, equipment: [])
+    params.require(:space).permit(:name, :host, :street, :city, :zipcode, :event_type, :place_type, :capacity, :description, :photo, equipment: [])
   end
 end

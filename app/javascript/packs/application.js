b9810156ca3e9d174e6f1f1458f3ff385ca20fef
Sarah Guichard <sarah.gcd1@gmail.com>
2019-08-22 18:29:35 +0200

    filters

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 import "bootstrap";
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
-flatpickr(".datepicker", {enableTime: true,
-    dateFormat: "Y-m-d H:i",})
+flatpickr(".datepicker", {enableTime: false,
+    dateFormat: "Y-m-d",})
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
